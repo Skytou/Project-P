d1c0eee9d1104343cbea6cc82a83d55b37df798b
Pradeep K <pradeepk@Pradeeps-MacBook-Pro.local>
2015-08-05 11:43:02 +0530

    added AI logic

--- Assets/Scripts/AIComponent.cs
+++ Assets/Scripts/AIComponent.cs
@@ -36,9 +36,18 @@ public class AIComponent : MonoBehaviour
 	string layerName;
 	float a_timer;
 
+	float idleDirection,moveDirection , prevMoveDirection;
+	
+	float xComponent;
+	float yComponent;
+	float angle;
+
+	 
+
 	void Awake()
 	{
 		playerRef = GameObject.FindGameObjectWithTag("Player");
+		aiAnimator = GetComponent<Animator>();
 
 	}
 
@@ -60,14 +69,123 @@ public class AIComponent : MonoBehaviour
 
 	}
 
+	void CalculateAngle(float angle, Vector3 target)
+	{
+		if(angle>0)
+		{
+			if(angle > 75 && angle <= 105)
+			{
+				// up 
+				moveDirection =1;
+				prevMoveDirection =1;
+				idleDirection =-1;
+				
+			}
+			else if(angle > 15 && angle <= 75)
+			{
+				// up right
+				moveDirection =2;
+				prevMoveDirection=2;
+				idleDirection =-1;
+				
+			}
+			else if(angle >= 0 && angle <= 15)
+			{
+				// right
+				moveDirection =3;
+				prevMoveDirection=3;
+				idleDirection =-1;
+			}
+			else if(angle > 105 && angle <= 165)
+			{
+				// up left
+				moveDirection =8;
+				prevMoveDirection=8;
+				idleDirection =-1;
+				
+			}
+			else //if(angle>165 && angle<=180)
+			{
+				// left
+				moveDirection =7;
+				prevMoveDirection=7;
+				idleDirection =-1;
+				
+			}
+			
+		}
+		
+		// Mapping angle to 8 directions 0 - -180
+		else
+		{
+			if(angle < -75 && angle >= -105)
+			{
+				// down
+				moveDirection =5;
+				prevMoveDirection=5;
+				idleDirection =-1;
+				
+			}
+			else if(angle < -15 && angle >= -75)
+			{
+				//down right
+				moveDirection =4;
+				prevMoveDirection=4;
+				idleDirection =-1;
+				
+			}
+			else if(angle <= 0 && angle >= -15)
+			{
+				// right
+				moveDirection =3;
+				prevMoveDirection=3;
+				idleDirection =-1;
+				
+			}
+			else if(angle < -105 && angle >= -165)
+			{
+				//down left
+				moveDirection =6;
+				prevMoveDirection=6;
+				idleDirection =-1;
+				
+			}
+			else //if(angle<-165 && angle>=-180)
+			{
+				// left 
+				moveDirection =7;
+				prevMoveDirection=7;
+				idleDirection =-1;
+				
+			}
+			
+		}
+	 
+		if(transform.position ==  playerRef.transform.position)
+		{
+			//isInMove = false;
+			//idleDirection = moveDirection;
+			idleDirection =prevMoveDirection;
+			moveDirection=-1;
+			
+			
+		}
+		aiAnimator.SetFloat("idleDirection",idleDirection);
+		aiAnimator.SetFloat("moveDirection",moveDirection);
+		
+	}
+
 	void MoveTowardsPlayer()
 	{
 		distanceToPlayer = Vector2.Distance(this.transform.position, playerRef.transform.position);
-		//Debug.Log(distanceToPlayer);
+		 
 
 		if(distanceToPlayer<distanceToAttack)
 		{
 			Stop();
+
+
+		//	Debug.Log("Calling stop");
 			// stop
 			// call hit animation for an interval
 
@@ -75,7 +193,14 @@ public class AIComponent : MonoBehaviour
 		else
 		{
 			this.transform.position = Vector2.MoveTowards(this.transform.position,playerRef.transform.position,aiMoveSpeed * Time.deltaTime);
+		 	xComponent = -transform.position.x + playerRef.transform.position.x;
+			yComponent = -transform.position.y + playerRef.transform.position.y;
+			angle = Mathf.Atan2(yComponent, xComponent) * Mathf.Rad2Deg;
+			CalculateAngle(angle, playerRef.transform.position);
+				
 		}
+
+		 
 	}
 
 
@@ -83,6 +208,11 @@ public class AIComponent : MonoBehaviour
 	{
 		// call idle animation
 		// call stop animation
+		moveDirection =-1;
+		idleDirection = prevMoveDirection;
+		aiAnimator.SetFloat("idleDirection",idleDirection);
+		aiAnimator.SetFloat("moveDirection",moveDirection);
+
 		if(a_timer <=0f)
 		{
 			// call Attack()
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,18 +9,20 @@ public class PlayerMovement : MonoBehaviour
 	private Vector3 target;
 	private Animator characterAnimator;
 	 
-	private AnimatorStateInfo animatorState;
+	private AnimatorStateInfo animatorStateInfo;
  
  
 	private Vector3 mouseClickPos;
 
-	int moveDirection;
+	float idleDirection,moveDirection , prevMoveDirection;
 	
 	float xComponent;
 	float yComponent;
 	float angle;
 
 	string layerName;
+
+	bool isInMove;
 
 	void Awake()
 	{
@@ -30,7 +32,10 @@ public class PlayerMovement : MonoBehaviour
 
 	void Start()
 	{
-
+		prevMoveDirection =5;
+		moveDirection=-1;
+		characterAnimator.SetFloat("idleDirection",idleDirection);
+		characterAnimator.SetFloat("moveDirection",moveDirection);
 	}
  
 
@@ -42,31 +47,42 @@ public class PlayerMovement : MonoBehaviour
 			{
 				// up 
 				moveDirection =1;
+				prevMoveDirection =1;
+				idleDirection =-1;
 
 			}
 			else if(angle > 15 && angle <= 75)
 			{
 				// up right
 				moveDirection =2;
+				prevMoveDirection=2;
+				idleDirection =-1;
 
 			}
 			else if(angle >= 0 && angle <= 15)
 			{
 				// right
 				moveDirection =3;
+				prevMoveDirection=3;
+				idleDirection =-1;
 			}
 			else if(angle > 105 && angle <= 165)
 			{
 				// up left
 				moveDirection =8;
+				prevMoveDirection=8;
+				idleDirection =-1;
 				 
 			}
-			else
+			else //if(angle>165 && angle<=180)
 			{
 				// left
 				moveDirection =7;
+				prevMoveDirection=7;
+				idleDirection =-1;
 				 
 			}
+			 
 		}
 		
 		// Mapping angle to 8 directions 0 - -180
@@ -76,37 +92,58 @@ public class PlayerMovement : MonoBehaviour
 			{
 				// down
 				moveDirection =5;
+				prevMoveDirection=5;
+				idleDirection =-1;
 				 
 			}
 			else if(angle < -15 && angle >= -75)
 			{
 				//down right
 				moveDirection =4;
+				prevMoveDirection=4;
+				idleDirection =-1;
 			 
 			}
 			else if(angle <= 0 && angle >= -15)
 			{
 				// right
 				moveDirection =3;
+				prevMoveDirection=3;
+				idleDirection =-1;
 				 
 			}
 			else if(angle < -105 && angle >= -165)
 			{
 				//down left
 				moveDirection =6;
+				prevMoveDirection=6;
+				idleDirection =-1;
 				 
 			}
-			else
+			else //if(angle<-165 && angle>=-180)
 			{
 				// left 
 				moveDirection =7;
+				prevMoveDirection=7;
+				idleDirection =-1;
 				 
 			}
+
 		}
+ 
+		//transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
 
-		characterAnimator.SetInteger("moveDirection",moveDirection);
+		if(transform.position ==  target)
+		{
+			  
+			idleDirection =prevMoveDirection;
+			moveDirection=-1;
 
-		transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+		}
+		characterAnimator.SetFloat("idleDirection",idleDirection);
+		characterAnimator.SetFloat("moveDirection",moveDirection);
+
 	}
 
 	 
@@ -118,6 +155,7 @@ public class PlayerMovement : MonoBehaviour
 
 	void Update()
 	{
+		animatorStateInfo = characterAnimator.GetCurrentAnimatorStateInfo (0);
 
 		//Movement();
 		if (Input.GetMouseButtonDown(0)) 
@@ -132,12 +170,15 @@ public class PlayerMovement : MonoBehaviour
 			angle = Mathf.Atan2(yComponent, xComponent) * Mathf.Rad2Deg;
 
 			//Debug.Log("Angle " + angle);
-
+			isInMove = true;
 	  		 
 		}
 
 
+		//if(isInMove)
 		CalculateAngle(angle , target);
+		transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
 	
 		//CallAnimation();
 	}    
