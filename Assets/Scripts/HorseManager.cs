d1a96cc5251fea7eb428eb7fb5d0faa89f916a7c
Pradeep K <pradeepk@Pradeeps-MacBook-Pro.local>
2015-08-17 19:59:05 +0530

    Added Tile..

--- Assets/Scripts/HorseManager.cs
+++ Assets/Scripts/HorseManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using Prime31;
 using System.Collections;
 
 public class HorseManager : MonoBehaviour 
@@ -12,39 +13,114 @@ public class HorseManager : MonoBehaviour
 
 	Rigidbody2D rBody;
 
+	public float gravity = -25f;
+	public float runSpeed = 8f;
+	public float groundDamping = 20f; // how fast do we change direction? higher means faster
+	public float inAirDamping = 5f;
+	public float jumpHeight = 3f;
+
+	[HideInInspector]
+	private float normalizedHorizontalSpeed = 0;
+
+	private CharacterController2D _controller;
+	private Animator _animator;
+	private RaycastHit2D _lastControllerColliderHit;
+	private Vector3 _velocity;
 
 
 	void Awake()
 	{
-		rBody = GetComponent<Rigidbody2D> ();
+		rBody =  GetComponent<Rigidbody2D> ();
+		_animator = horseRef.GetComponent<Animator>();
+		_controller = GetComponent<CharacterController2D>();
+
+		// listen to some events for illustration purposes
+		_controller.onControllerCollidedEvent += onControllerCollider;
+		_controller.onTriggerEnterEvent += onTriggerEnterEvent;
+		_controller.onTriggerExitEvent += onTriggerExitEvent;
 	}
 	// Use this for initialization
 	void Start () 
 	{
 	
 	}
-	
+
+	#region Event Listeners
+
+	void onControllerCollider( RaycastHit2D hit )
+	{
+		// bail out on plain old ground hits cause they arent very interesting
+		if( hit.normal.y == 1f )
+			return;
+
+		// logs any collider hits if uncommented. it gets noisy so it is commented out for the demo
+		//Debug.Log( "flags: " + _controller.collisionState + ", hit.normal: " + hit.normal );
+	}
+
+
+	void onTriggerEnterEvent( Collider2D col )
+	{
+		Debug.Log( "onTriggerEnterEvent: " + col.gameObject.name );
+	}
+
+
+	void onTriggerExitEvent( Collider2D col )
+	{
+		Debug.Log( "onTriggerExitEvent: " + col.gameObject.name );
+	}
+
+	#endregion
+
+	void HorseMovement()
+	{
+		if( _controller.isGrounded )
+			_velocity.y = 0;
+
+		 
+		 
+			normalizedHorizontalSpeed = 1;
+
+		if( _controller.isGrounded )
+				_animator.Play( Animator.StringToHash( "HorseRun" ) );
+		
+
+
+		// we can only jump whilst grounded
+		if( _controller.isGrounded && Input.GetKeyDown( KeyCode.Space ) )
+		{
+			_velocity.y = Mathf.Sqrt( 2f * jumpHeight * -gravity );
+
+			_animator.Play (Animator.StringToHash ("HorseJump"));
+			//_animator.Play( Animator.StringToHash( "Jump" ) );
+		}
+
+
+		// apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
+		var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
+		_velocity.x = Mathf.Lerp( _velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor );
+
+		// apply gravity before moving
+		_velocity.y += gravity * Time.deltaTime;
+
+		// if holding down bump up our movement amount and turn off one way platform detection for a frame.
+		// this lets uf jump down through one way platforms
+		if( _controller.isGrounded && Input.GetKey( KeyCode.DownArrow ) )
+		{
+			_velocity.y *= 3f;
+			_controller.ignoreOneWayPlatformsThisFrame = true;
+		}
+
+		_controller.move( _velocity * Time.deltaTime );
+
+		// grab our current _velocity to use as a base for all calculations
+		_velocity = _controller.velocity;
+	}
+
 	// Update is called once per frame
 	void Update () 
 	{
 
-		//rBody.velocity = new Vector2 (speed, this.gameObject.transform.position.y);
-		//rBody.AddForce(transform.right * speed ,ForceMode2D.Impulse);
-		//rBody.MovePosition (Vector2.right * speed * Time.deltaTime );
-
-
-
-		if(Input.GetKey(KeyCode.Space))
-		{
-			Debug.Log ("Space");
-			this.gameObject.transform.Translate (Vector3.up * speed * Time.deltaTime, Space.Self);
-		}
-
-		//else
-		{
-			this.gameObject.transform.Translate (Vector3.right * speed * Time.deltaTime, Space.Self);
-
-		}
+		HorseMovement();
 	
 	}
 }
