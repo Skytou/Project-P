8cbbdcbe3ae25da03859f5e0902b2998e47903e4
Dharmaraj P <dharmaraj@skytousolution.com>
2015-09-11 15:10:52 +0530

    Horse - Implemented Infinite Runner

---
+++ Assets/Scripts/Horse/Pooler.cs
@@ -0,0 +1,150 @@
+﻿/**
+Script Author : Vaikash 
+Description   : Generic Object Pooler
+**/
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pooler : MonoBehaviour
+{
+
+    #region Var
+
+    public static Pooler InstRef;
+
+    //public int poolerObjLimit;
+    public PoolerData[] PoolHolder;
+
+    GameObject instance;
+
+    [System.Serializable]
+    public struct PoolerData
+    {
+        public string name;
+        public int ID; // starts from 0
+        public int cloneLimit;
+        public bool canGrow;
+        //public bool isBusy; // to be implemented if need arises
+        public GameObject prefab;
+        public List<GameObject> pool;
+    }
+
+    #endregion Var
+
+    public void Awake()
+    {
+        InstRef = this;
+    }
+
+    void Start()
+    {
+
+    }
+
+    public void OnEnable()
+    {
+        Init();
+    }
+
+    public void OnDisable()
+    {
+        ShutDown();
+    }
+
+    void Update()
+    {
+
+    }
+
+    // Initialize Pooler
+    void Init()
+    {
+        foreach (var obj in PoolHolder)
+        {
+            PoolInit(obj);
+        }
+    }
+
+    // Clean Pooler
+    void ShutDown()
+    {
+        foreach (var obj in PoolHolder)
+        {
+            PoolKill(obj);
+        }
+    }
+
+    // Create, Store and Hide
+    void PoolInit(PoolerData data)
+    {
+        for (int i = 0; i < data.cloneLimit; i++)
+        {
+            instance=(GameObject)Instantiate(data.prefab, Vector3.zero, data.prefab.transform.rotation);
+            instance.transform.parent = transform;
+            data.pool.Add(instance);
+            instance.SetActive(false);
+        }
+    }
+
+    // Destroy and Clean
+    void PoolKill(PoolerData data)
+    {
+        for (int i = 0; i < data.pool.Count; i++)
+        {
+            Destroy(data.pool[i]);
+        }
+        data.pool.Clear();
+    }
+
+    #region Interface
+
+    // Get a specific type of pooled object
+    public GameObject GetPooledObject(int ID)
+    {
+        GameObject polledInst = null;
+        var pooler=PoolHolder[ID];
+        for (int i = 0; i < pooler.pool.Count; i++)
+        {
+            if(!pooler.pool[i].activeInHierarchy)
+            {
+                polledInst = pooler.pool[i];
+                //polledInst.SetActive(true);
+                return polledInst;
+            }
+        }
+        if (pooler.canGrow)
+        {
+            instance=(GameObject)Instantiate(pooler.prefab, Vector3.zero, pooler.prefab.transform.rotation);
+            instance.transform.parent = this.gameObject.transform;
+            pooler.pool.Add(instance);
+            //instance.SetActive(true);
+            return instance;
+        }
+        return null;
+    }
+
+    // Pseudo Destroy Code
+    public void Sleep(GameObject Obj)
+    {
+        //Obj.transform.position = Vector3.zero;
+        //Obj.transform.rotation = Quaternion.identity;
+        Obj.SetActive(false);
+    }
+
+    // Disable all pooled Objects
+    public void HideAll()
+    {
+        foreach (var obj in PoolHolder)
+        {
+            for (int i = 0; i < obj.pool.Count; i++)
+            {
+                var pooledObj=obj.pool[i];
+                pooledObj.SetActive(false);
+            }
+        }
+    }
+
+    #endregion Interface
+}
---
+++ Assets/Scripts/Horse/SpawnTrigger.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnTrigger : MonoBehaviour
+{
+    #region Var
+
+    public static int prevPlat;
+    public static int beforePrevPlat;
+    public static int twoBeforePrevPlat;
+    public Vector3[] distBtnPlatforms; //  starts from index 1
+
+    int randNo;
+    GameObject instance;
+
+    #endregion var
+
+    void Start()
+    {
+        prevPlat = 1;
+    }
+
+    void Update()
+    {
+
+    }
+
+    #region SpawnHandler
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Trigger");
+            randNo = 1;// Random.Range(1, 2);
+
+            instance = Pooler.InstRef.GetPooledObject(randNo);
+            instance.transform.position = transform.position + distBtnPlatforms[1];
+            instance.SetActive(true);
+
+            // Update spawn history
+            twoBeforePrevPlat = beforePrevPlat;
+            beforePrevPlat = prevPlat;
+            prevPlat = randNo;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" /*&& DogRunner.instRef.runStart*/)//check - is game running if needed 
+        {
+            Pooler.InstRef.Sleep((gameObject.transform.parent.gameObject));
+        }
+    }
+
+    #endregion SpawnHandler
+}
