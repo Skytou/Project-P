0a1174af793cda6ab329dfb10c7729f80b82433e
ponniah-skytou <ponniah@skytousolution.com>
2015-09-17 18:04:31 +0530

    Bonus mgr

---
+++ Assets/Scripts/InApp/BonusMgr.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BonusMgr : MonoBehaviour {
+
+    float BonusTime = 10; // bonus after 10 seconds
+    float prevTime = 0;
+    float curTime = 0;
+
+    public Text BonusText;
+    public Text PrevTimeText;
+    public Text CurTimeText;
+
+	void Start () {
+    }
+	
+
+	void Update () {
+        BonusTime -= Time.deltaTime;
+        if (BonusTime < 0)
+        {
+            BonusTime = 10;
+            prevTime = curTime;
+            curTime++;
+        }
+        int BonusTimeInt = (int)BonusTime;
+        BonusText.text = "B: " + BonusTimeInt.ToString();
+        PrevTimeText.text = "p : " + prevTime.ToString();
+        CurTimeText.text = "c : " + curTime.ToString();
+    }
+
+
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.SetFloat("curTime", curTime);
+        PlayerPrefs.SetFloat("BonusTime", BonusTime);
+        PlayerPrefs.Save();
+    }
+
+}
