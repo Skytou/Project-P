1769e2274dfd1f072b853cfeb027697ba2d6f5fa
ponniah-skytou <ponniah@skytousolution.com>
2015-09-18 14:13:03 +0530

    added open app plugin

---
+++ Assets/Scripts/InApp/OpenAppAndroid.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class OpenAppAndroid : MonoBehaviour
+{
+
+    bool isGalleryImageLoaded = false;
+    public Image curImg;
+    List<string> apps;
+    int curAppIndex = 0;
+    public Text curAppText;
+
+    void Start()
+    {
+        // case sensitive
+        apps = new List<string>();
+        apps.Add("com.SkyTou.PuppyWorld");
+        apps.Add("com.znop.zigzagnoodles");
+        apps.Add("com.skyTou.puppyWorld");
+        apps.Add("com.Znop.HexaChains");
+        curAppText.text = "Open[" + curAppIndex + "]" + apps[curAppIndex];
+    }
+
+
+    void Update()
+    {
+        if (isGalleryImageLoaded)
+        {
+
+        }
+    }
+
+
+    public void OnOpen()
+    {
+        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+        bool chkVal = currentActivity.Call<bool>("appInstalledOrNot", "com.SkyTou.PuppyWorld"); //com.SkyTou.PuppyWorld
+        Debug.Log("chkVal : " + chkVal);
+    }
+
+
+    public void OncallShareApp()
+    {
+        string subject = "WORD-O-MAZE";
+        string body = "PLAY THIS AWESOME. GET IT ON THE PLAYSTORE";
+
+        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+        currentActivity.Call("shareText", subject, body);
+    }
+
+
+    public void OnOpenApp()
+    {
+        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+        currentActivity.Call("openApp", apps[curAppIndex]); //com.SkyTou.PuppyWorld
+
+        curAppIndex++;
+        if (curAppIndex >= apps.Count)
+        {
+            curAppIndex = 0;
+        }
+        curAppText.text = "Open[" + curAppIndex + "]" + apps[curAppIndex];
+    }
+
+
+    public void OnPhotoPick(string filePath)
+    {
+        Debug.Log("OnPhotoPick : " + filePath);
+    }
+}
