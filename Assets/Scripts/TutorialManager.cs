622fb66258298469c3bc88e91c85d6bcd7b8fbb8
Dharmaraj P <dharmaraj@skytousolution.com>
2015-09-25 17:27:26 +0530

    Tutorial Scene-Images and Icons Added

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialManager : MonoBehaviour {
+    public GameObject [] TutImg;
+    public bool canClick;
+    public GameObject ForwardBtn;
+    public GameObject Backbutton;
+    int count;
+
+
+
+
+	
+	void Start ()
+    {
+       
+	}
+	
+	
+	void Update ()
+    {
+	
+	}
+    public void PointerDownForward()
+    {
+       
+        
+        TutImg[count].SetActive(false);
+        count += 1;
+        if (count > 3)
+        {
+            count = 3;
+        }
+        TutImg[count].SetActive(true);
+        
+
+    }
+    public void PointerDownBack()
+    {
+        
+        {
+            TutImg[count].SetActive(false);
+            count -= 1;
+            if(count<0)
+            {
+                count = 0;
+            }
+            TutImg[count].SetActive(true);
+        }
+    }
+}
