95cf9c698cbb7b89eee36f441fc6a4c1024c57ff
ponniah-skytou <ponniah@skytousolution.com>
2015-09-25 18:31:28 +0530

    tutorial scene

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -7,46 +7,41 @@ public class TutorialManager : MonoBehaviour {
     public GameObject ForwardBtn;
     public GameObject Backbutton;
     int count;
-
-
-
-
 	
 	void Start ()
     {
-       
 	}
 	
 	
 	void Update ()
     {
-	
 	}
+
+
     public void PointerDownForward()
     {
-       
-        
         TutImg[count].SetActive(false);
         count += 1;
+        if (count >= 4)
+        {
+            Application.LoadLevel(GameGlobalVariablesManager.MainMenu);
+        }
         if (count > 3)
         {
             count = 3;
         }
         TutImg[count].SetActive(true);
-        
-
     }
+
+
     public void PointerDownBack()
     {
-        
+        TutImg[count].SetActive(false);
+        count -= 1;
+        if(count<0)
         {
-            TutImg[count].SetActive(false);
-            count -= 1;
-            if(count<0)
-            {
-                count = 0;
-            }
-            TutImg[count].SetActive(true);
+            count = 0;
         }
+        TutImg[count].SetActive(true);
     }
 }
