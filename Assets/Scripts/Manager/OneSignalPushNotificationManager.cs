0fe7d1c30f4c8189c5f2cd491dca0f6a68047a85
Pradeep K <pradeepk@Pradeeps-MacBook-Pro.local>
2015-10-12 16:28:30 +0530

    Added Push Notification

---
+++ Assets/Scripts/Manager/OneSignalPushNotificationManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+
+public class OneSignalPushNotificationManager : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log ("Registered for push");
+		OneSignal.Init ("65870fdc-70cf-11e5-b414-c77cb008632e","419912205759",HandleNotification);
+	}
+
+	private static void HandleNotification(string message, Dictionary<string, object> additionalData, bool isActive) {
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
